312363488a37cd4a5d288c5dea172204794b45c4
sinhaujjwalkumar1-cell <sinhaujjwalkumar1@gmail.com>
2025-09-21 22:12:03 +0530

    Create servicedetail.js

---
+++ servicedetail.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ServiceDetail = () => {
+  return (
+    <div>
+      <h2>Service Details</h2>
+      {/* TODO: Show detailed service & provider Info */}
+    </div>
+  );
+};
+
+export default ServiceDetail;
