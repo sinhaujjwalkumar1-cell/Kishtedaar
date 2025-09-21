f1b18083b4bd9aa5957240ed78498b5a6e42b6d1
sinhaujjwalkumar1-cell <sinhaujjwalkumar1@gmail.com>
2025-09-21 22:11:02 +0530

    Create profile.js

---
+++ profile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div>
+      <h2>My Profile</h2>
+      {/* TODO: Implement user profile view and edit */}
+    </div>
+  );
+};
+
+export default Profile;
